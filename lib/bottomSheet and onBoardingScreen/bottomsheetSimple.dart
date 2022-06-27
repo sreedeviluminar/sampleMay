6fef8303f3e1ee9207f12b95046ce5f5f603eb12
Sreedevi <sreedevi@luminartechnolab.com>
2022-06-27 11:57:35 +0530

    commit

---
+++ lib/bottomSheet and onBoardingScreen/bottomsheetSimple.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Bottom Sheet',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState()
+  {
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Simple Bottom Sheet"),
+      ),
+      body:Center(
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  _showModalBottomSheet();
+                },
+                child: const Text("Show Bottom Sheet"),
+              )
+            ],
+          )
+      ),
+    );
+  }
+  _showModalBottomSheet() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          height: 200,
+          width: double.infinity,
+          color: Colors.grey.shade200,
+          alignment: Alignment.center,
+          child: ElevatedButton(
+            child: const Text("Close Bottom Sheet"),
+            style: ElevatedButton.styleFrom(
+              onPrimary: Colors.white,
+              primary: Colors.green,
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/grid and list/Tables.dart
+++ lib/grid and list/Tables.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyTable extends StatelessWidget {
+  const MyTable({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
