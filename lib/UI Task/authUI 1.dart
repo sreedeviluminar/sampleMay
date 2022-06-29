ce106383c5ac73a4c8095d821a7bb27468287010
Sreedevi <sreedevi@luminartechnolab.com>
2022-06-29 12:58:21 +0530

    commit

--- lib/UI Task/authUI 1.dart
+++ lib/UI Task/authUI 1.dart
@@ -1,11 +1,15 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-void main(){
-  runApp(MaterialApp(home: AuthThreePage(),));
+
+void main() {
+  runApp(MaterialApp(
+    home: AuthThreePage(),
+  ));
 }
 
 class AuthThreePage extends StatefulWidget {
-
   @override
   _AuthThreePageState createState() => _AuthThreePageState();
 }
@@ -26,170 +30,191 @@ class _AuthThreePageState extends State<AuthThreePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/room4.jpg'),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Stack(
-            children: <Widget>[
-              Container(
-                color: Colors.black54,
-                child: Column(
-                  children: <Widget>[
-                    const SizedBox(height: kToolbarHeight + 40),
-                    Expanded(
-                      child: Column(
-                        children: const <Widget>[
-                          Text(
-                            "Welcome",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500,
-                              fontSize: 30.0,
-                            ),
-                          ),
-                          SizedBox(height: 10.0),
-                          Text(
-                            "Welcome to this awesome login app. \n You are awesome",
-                            style: TextStyle(
-                              color: Colors.white70,
-                              fontSize: 18.0,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 10.0),
-                    Row(
-                      children: <Widget>[
-                        const SizedBox(width: 10.0),
-                        Expanded(
-                          child: RaisedButton(
-                            color: Colors.red,
-                            textColor: Colors.white,
-                            elevation: 0,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20.0),
-                            ),
-                            child: Text("Login"),
-                            onPressed: () {
-                              setState(() {
-                                formVisible = true;
-                                _formsIndex = 1;
-                              });
-                            },
-                          ),
-                        ),
-                        const SizedBox(width: 10.0),
-                        Expanded(
-                          child: RaisedButton(
-                            color: Colors.grey.shade700,
-                            textColor: Colors.white,
-                            elevation: 0,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20.0),
-                            ),
-                            child: Text("Signup"),
-                            onPressed: () {
-                              setState(() {
-                                formVisible = true;
-                                _formsIndex = 2;
-                              });
-                            },
-                          ),
-                        ),
-                        const SizedBox(width: 10.0),
-                      ],
-                    ),
-                    const SizedBox(height: 40.0),
-                    OutlineButton.icon(
-                      borderSide: BorderSide(color: Colors.red),
-                      color: Colors.red,
-                      textColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                      icon: Icon(FontAwesomeIcons.google),
-                      label: Text("Continue with Google"),
-                      onPressed: () {},
-                    ),
-                    const SizedBox(height: 20.0),
-                  ],
-                ),
-              ),
-              AnimatedSwitcher(
-                duration: Duration(milliseconds: 200),
-                child: (!formVisible)
-                    ? null
-                    : Container(
-                  color: Colors.black54,
-                  alignment: Alignment.center,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/room4.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            color: Colors.black54,
+            child: Column(
+              children: <Widget>[
+                const SizedBox(height: kToolbarHeight + 40),
+                Expanded(
                   child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          RaisedButton(
-                            textColor: _formsIndex == 1
-                                ? Colors.white
-                                : Colors.black,
-                            color:
-                            _formsIndex == 1 ? Colors.red : Colors.white,
-                            child: Text("Login"),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20.0)),
-                            onPressed: () {
-                              setState(() {
-                                _formsIndex = 1;
-                              });
-                            },
-                          ),
-                          const SizedBox(width: 10.0),
-                          RaisedButton(
-                            textColor: _formsIndex == 2
-                                ? Colors.white
-                                : Colors.black,
-                            color:
-                            _formsIndex == 2 ? Colors.red : Colors.white,
-                            child: Text("Signup"),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20.0)),
-                            onPressed: () {
-                              setState(() {
-                                _formsIndex = 2;
-                              });
-                            },
-                          ),
-                          const SizedBox(width: 10.0),
-                          IconButton(
-                            color: Colors.white,
-                            icon: Icon(Icons.clear),
-                            onPressed: () {
-                              setState(() {
-                                formVisible = false;
-                              });
-                            },
-                          )
-                        ],
-                      ),
-                      Container(
-                        child: AnimatedSwitcher(
-                          duration: Duration(milliseconds: 300),
-                          child:
-                          _formsIndex == 1 ? LoginForm() : SignupForm(),
+                    children: const <Widget>[
+                      Text(
+                        "Welcome",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30.0,
                         ),
-                      )
+                      ),
+                      SizedBox(height: 10.0),
+                      Text(
+                        "Welcome to this awesome login app. \n You are awesome",
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 18.0,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
                     ],
                   ),
                 ),
-              )
-            ],
+                const SizedBox(height: 10.0),
+                Row(
+                  children: <Widget>[
+                    const SizedBox(width: 10.0),
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.red,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                        child: const Text("Login"),
+                        onPressed: () {
+                          setState(() {
+                            formVisible = true;
+                            _formsIndex = 1;
+                          });
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 10.0),
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.grey.shade700,
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            )),
+                        child: const Text(
+                          "Signup",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            formVisible = true;
+                            _formsIndex = 2;
+                          });
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 10.0),
+                  ],
+                ),
+                const SizedBox(height: 40.0),
+                OutlinedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(FontAwesomeIcons.google),
+                  label: const Text(
+                    "Continue with Google",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Colors.red),
+                    backgroundColor: Colors.transparent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+              ],
+            ),
           ),
-        ));
+
+
+          AnimatedSwitcher(
+            duration: Duration(milliseconds: 200),
+            child: (!formVisible)
+                ? null
+                : Container(
+                    color: Colors.black54,
+                    alignment: Alignment.center,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: _formsIndex == 1
+                                    ? Colors.red
+                                    : Colors.white,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0)),
+                              ),
+                              child: Text(
+                                "Login",
+                                style: TextStyle(
+                                  color: _formsIndex == 1
+                                      ? Colors.white
+                                      : Colors.black,
+                                ),
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _formsIndex = 1;
+                                });
+                              },
+                            ),
+                            const SizedBox(width: 10.0),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: _formsIndex == 2
+                                    ? Colors.red
+                                    : Colors.white,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0)),
+                              ),
+                              child: Text("Signup",
+                                  style: TextStyle(
+                                    color: _formsIndex == 1
+                                        ? Colors.white
+                                        : Colors.black,
+                                  )),
+                              onPressed: () {
+                                setState(() {
+                                  _formsIndex = 2;
+                                });
+                              },
+                            ),
+                            const SizedBox(width: 10.0),
+                            IconButton(
+                              color: Colors.white,
+                              icon: Icon(Icons.clear),
+                              onPressed: () {
+                                setState(() {
+                                  formVisible = false;
+                                });
+                              },
+                            )
+                          ],
+                        ),
+                        Container(
+                          child: AnimatedSwitcher(
+                            duration: Duration(milliseconds: 300),
+                            child: _formsIndex == 1 ? LoginForm() : SignupForm(),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+          )
+        ],
+      ),
+    ));
   }
 }
 
@@ -210,14 +235,14 @@ class LoginForm extends StatelessWidget {
         shrinkWrap: true,
         padding: const EdgeInsets.all(16.0),
         children: <Widget>[
-          TextField(
+          const TextField(
             decoration: InputDecoration(
               hintText: "Enter email",
               border: OutlineInputBorder(),
             ),
           ),
           const SizedBox(height: 10.0),
-          TextField(
+          const TextField(
             obscureText: true,
             decoration: InputDecoration(
               hintText: "Enter password",
@@ -242,9 +267,7 @@ class LoginForm extends StatelessWidget {
 }
 
 class SignupForm extends StatelessWidget {
-  const SignupForm({
-    Key? key,
-  }) : super(key: key);
+  const SignupForm({Key? key,}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -258,14 +281,14 @@ class SignupForm extends StatelessWidget {
         shrinkWrap: true,
         padding: const EdgeInsets.all(16.0),
         children: <Widget>[
-          TextField(
+          const TextField(
             decoration: InputDecoration(
               hintText: "Enter email",
               border: OutlineInputBorder(),
             ),
           ),
           const SizedBox(height: 10.0),
-          TextField(
+          const TextField(
             obscureText: true,
             decoration: InputDecoration(
               hintText: "Enter password",
@@ -273,7 +296,7 @@ class SignupForm extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 10.0),
-          TextField(
+          const TextField(
             obscureText: true,
             decoration: InputDecoration(
               hintText: "Confirm password",
@@ -281,14 +304,15 @@ class SignupForm extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 10.0),
-          RaisedButton(
-            color: Colors.red,
-            textColor: Colors.white,
-            elevation: 0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20.0),
-            ),
-            child: Text("Signup"),
+         ElevatedButton(
+           style: ElevatedButton.styleFrom(
+             primary: Colors.red,
+             elevation: 0,
+             shape: RoundedRectangleBorder(
+               borderRadius: BorderRadius.circular(20.0),
+             ),
+           ),
+            child: const Text("Signup",style: TextStyle(color: Colors.white),),
             onPressed: () {},
           ),
         ],
